708d6618b05a5d90ec6b06e4a564c3a70d87d31d
marwinious <marwinious@gmail.com>
2012-03-15 14:04:19 -0400

    Re-arranged some elements in the initial index.php and copy_paste_me.php file for better PHP structuring

--- copy_paste_me.php
+++ copy_paste_me.php
@@ -1,18 +1,23 @@
 <?PHP
-define("PAGE_TITLE","NEW PAGE");
+// INCLUDES
 require("config.php");
 
+// CACHING, IF ENABLED
 if($enable_cache) {
 	// CHECK FOR CACHED PAGE
 	$cache_loaded = false;
 	if(performance::cache_load("index")) { $cache_loaded = true; exit();}
 }
 
+// PAGE TITLE
+define("PAGE_TITLE","NEW PAGE");
+
+// LOAD HEADER
 require("header.php");
 ?>
 
 <section id="main">
-
+	
 </section>
 
 <script type="text/javascript">
@@ -24,8 +29,10 @@ $(document).ready(function() {
 </script>
 
 <?PHP 
+// LOAD FOOTER
 require("footer.php");
 
+// CACHING, IF ENABLED
 if($enable_cache) {
 	// SAVE PAGE TO CACHE
 	if(!$cache_loaded) { performance::cache_save("cache"); }
--- index.php
+++ index.php
@@ -1,20 +1,23 @@
 <?PHP
-define("PAGE_TITLE","Home");
+// INCLUDES
 require("config.php");
 
+// CACHING, IF ENABLED
 if($enable_cache) {
 	// CHECK FOR CACHED PAGE
 	$cache_loaded = false;
 	if(performance::cache_load("index")) { $cache_loaded = true; exit();}
 }
 
+// PAGE TITLE
+define("PAGE_TITLE","Home");
+
+// LOAD HEADER
 require("header.php");
 ?>
 
 <section id="main">
-	<?PHP
 	
-	?>
 </section>
 
 <script type="text/javascript">
@@ -26,8 +29,10 @@ $(document).ready(function() {
 </script>
 
 <?PHP 
+// LOAD FOOTER
 require("footer.php");
 
+// CACHING, IF ENABLED
 if($enable_cache) {
 	// SAVE PAGE TO CACHE
 	if(!$cache_loaded) { performance::cache_save("cache"); }
